abf48da1220482bd517cd02d8a6914d343643487
tanghuitre <tanvelaz06@gmail.com>
2025-11-30 12:29:49 +0100

    beaucoup de copier coller... pas confiant XD
    Implemented phyloDNADistance function to calculate DNA distance and phyloTreeCreate function to build a phylogenetic tree from a file.

--- Phylogenetic.c
+++ Phylogenetic.c
@@ -1,14 +1,93 @@
-#include <stdio.h>
+ #include <stdio.h>
 #include <stdlib.h>
+#include <math.h">
 #include "Phylogenetic.h"
+// exactement le nombre de caractere utilisé dans main-feature
+#define MAXLINELENGTH 2000
 
 double phyloDNADistance(char *dna1, char *dna2){
-    // TODO
-    return 0;
+    unsigned long longeur = 0 ;
+    unsigned long P = 0 ;
+    unsigned long Q = 0 ;
+
+    if(strlen(dna1)<strlen(dna2))
+        longeur = strlen(dna1);
+    else
+        longeur = strlen(dna2);
+
+    for (unsigned long i = 0 ; i < longeur ; i++){
+        char a = dna1[i]
+        char b = dna2[i]
+        if(a==b)
+            continue;
+        if((a=='A'||b=='A') && (b=='G'||a=='G')){
+            Q++ ;
+            continue;
+        }
+        if((a=='C'||b=='C') && (b=='T'||a=='T')){
+            Q++;
+            continue
+        }
+        P++;
+    }
+    
+    double distance = ((-1/2)*log(1-2*P-Q))-((1/4)*log(1-2*Q));//log == ln ?
+    return distance;
 }
 
 
 Hclust *phyloTreeCreate(char *filename){
-    // TODO
-    return 0;
+    char buffer [MAXLINELENGTH];
+    FILE *fp = fopen(filename, "r");
+    fgets(buffer, MAXLINELENGTH, fp);
+    int nbInfo = 2 ;
+    // on nous le dit il y a 1 nom d'espece et un ADN s'aparé par une ',' donc 2 info par espece
+
+    List *names = llCreateEmpty();
+    Dict *dicfeatures = dictCreate(1000);
+
+        while (fgets(buffer, MAXLINELENGTH, fp))
+    {
+        int lenstr = strlen(buffer) - 1;
+        buffer[lenstr] = '\0'; // replace \n with \0
+
+        // jusqu'a le premier ',' (nom)
+        int i = 0;
+        while (buffer[i] != ',')
+            i++;
+        buffer[i] = '\0';
+
+        // gestion du nom
+        char *objectName = malloc((i + 1) * sizeof(char));
+        strcpy(objectName, buffer);
+        llInsertLast(names, objectName);
+
+        double *featureVector = malloc(nbInfo * sizeof(double));
+        int pos = 0;
+        i++;
+        // la construction suivante est adapté a dictINseret que je n'ai pas ecrite 
+        // elle doit surement etre juste 
+        while (i < lenstr && pos < nbInfo)
+        {
+            int j = i;
+            while (j < lenstr && buffer[j] != ',')
+                j++;
+            if (buffer[j] == ',')
+                buffer[j] = '\0';
+            featureVector[pos] = atof(buffer + i);
+            pos++;
+            i = j + 1;
+        }
+        dictInsert(dicfeatures, objectName, featureVector);
+    }
+    Hclust *hc = hclustBuildTree(names, phyloDNADistance, &pf);
+    // free the memory
+    dictFreeValues(nbInfo, free);
+    llFreeData(names);
+    fclose(fp);
+
+    return hc;
 }
+
+// j'ai enlevé les sercurité pour y voir plus claire 
+// mais ca semblait etre que du copier coller :/ reverifie au cas ou 
